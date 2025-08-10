4210c9839a132ca99bf861651e0e6406c27eeb7e
Kimjihun <125746081+zhun0922@users.noreply.github.com>
2025-08-11 00:14:45 +0900

    create queue

---
+++ BarkingDogCpp/BarkingDogCpp/6_queue.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MX = 1000005;
+int dat[MX];
+int head = 0, tail = 0;
+
+void push(int x) {
+	dat[tail++] = x;
+}
+
+void pop() {
+	if(head < tail) head++;
+}
+
+int front() {
+	return dat[head];
+}
+
+int back() {
+	return dat[tail - 1];
+}
+
+void traverse() {
+	for (int i = head; i < tail; i++) {
+		cout << dat[i];
+	}
+}
+
+
+int main() {
+	push(10); push(20); push(30);
+	cout << front() << '\n'; // 10
+	cout << back() << '\n'; // 30
+	pop(); pop();
+	push(15); push(25);
+	cout << front() << '\n'; // 30
+	cout << back() << '\n'; // 25
+}
