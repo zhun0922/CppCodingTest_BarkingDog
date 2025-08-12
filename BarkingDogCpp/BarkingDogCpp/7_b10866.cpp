728ce53e6b1fca8fca3e0adb6902692f3e21c039
Kimjihun <125746081+zhun0922@users.noreply.github.com>
2025-08-12 22:14:13 +0900

    create 10866

---
+++ BarkingDogCpp/BarkingDogCpp/7_b10866.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+
+int x; 
+int main() {
+	int n = 0;
+	cin >> n;
+
+	deque<int> DQ;
+	while (n--) {
+		string s = "";
+		cin >> s;
+		if (s == "push_front") {
+			cin >> x;
+			DQ.push_front(x);
+		}
+		else if (s == "push_back") {
+			cin >> x;
+			DQ.push_back(x);
+		}
+		else if (s == "pop_front") {
+			if (DQ.empty()) { cout << "-1" << '\n'; continue; }
+			cout << DQ.front() << '\n';
+			DQ.pop_front();
+		}
+		else if (s == "pop_back") {
+			if (DQ.empty()) { cout << "-1" << '\n'; continue;}
+			cout << DQ.back() << '\n';
+			DQ.pop_back();
+		}
+		else if (s == "size") {
+			cout << DQ.size() << '\n';
+		}
+		else if (s == "empty") {
+			if (DQ.empty()){ cout << "1" << '\n'; continue;}
+			else cout << "0" << '\n';
+		}
+		else if (s == "front") {
+			if (DQ.empty()) { cout << "-1" << '\n'; continue;}
+			cout << DQ.front() << '\n';
+		}
+		else if (s == "back") {
+			if (DQ.empty()) { cout << "-1" << '\n'; continue;}
+			cout << DQ.back() << '\n';
+		}
+	}
+}
